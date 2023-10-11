3b037eac73c335980541835b65f03f8d2c13acbb
JayaKeerthan01 <jjayakeerthan@gmail.com>
2023-10-11 19:17:00 +0530

    updated

---
+++ requirements.js
@@ -0,0 +1,50 @@
+//Requirement for our project
+
+//we are a book management company
+
+//BOOKS
+//ISBN, title, pub date, language, num page, author[], category[]
+
+//AUTHORS
+//id, name, books[]
+
+//PUBLICATIONS
+//id, name, books[]
+
+//we have to design and code an API over this  
+
+//1.BOOKS
+//we need an API :-
+//To get all the books-DONE
+//To get specific book-DONE
+//To get list of books based on category-DONE
+//To get list of books based on languages-DONE
+
+//2.AUTHORS
+//we need an API :-
+//To get all the authors-DONE
+//To get a specific author based on id-DONE
+//To get a list of authors based on books-DONE
+
+//3.PUBLICATIONS
+//we need an API :-
+//To get all the publications-DONE
+//To get a specific publications-DONE
+//To get a list of publications based on books-DONE
+
+//POST REQUEST
+//1.ADD NEW BOOK-DONE
+//1.ADD NEW AUTHOR-DONE
+//1.ADD NEW PUBLICATION-DONE
+ 
+
+//PUT
+//Update book details if author is changed-DONE
+
+//DELETE
+//1.DELETE A BOOK
+//2.DELETE AUTHOR FROM BOOK
+//3.DELETE AUTHOR FROM BOOK AND RELATED BOOK FROM AUTHOR
+
+
+
